994c12c97fc6e8597c7f24b0daff3d43010c4c94
anujtheprogrammer <anujbhatt927@gmail.com>
2021-03-28 02:13:49 +0530

    a minor change done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("API Running");
+    res.send("API Running successfully");
 });
 
 const PORT = process.env.PORT || 5000;
