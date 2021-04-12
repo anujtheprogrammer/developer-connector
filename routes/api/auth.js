72f99db45085848c0036caf67fc00959f1b3b6e9
anujtheprogrammer <anujbhatt927@gmail.com>
2021-04-12 20:00:12 +0530

    login user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check,validationResult} = require('express-validator');
 const User = require('../../models/Users')
 
 // @router   get api/auth
@@ -15,6 +18,57 @@ router.get('/', auth, async (req,res)=>{
         console.err(err.message);
         res.status(500).send('server error');
     }
+});
+
+// @router   POST api/auth
+// @desc     register user & get token
+// @access   public
+router.post('/', [
+
+    check('email','please enter a valid email address').isEmail(),
+    check('password','password should be min 6 character long').exists()
+
+], async (req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array() });
+    }
+
+    const { email, password} = req.body;
+
+    try{
+        // user should not already exist
+        let user = await User.findOne({ email });
+        if(!user){
+            res.status(400).json({errors: [{msg: 'invalid credentials' }] });
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            res.status(400).json({errors: [{msg: 'invalid credentials' }] }); 
+        }
+        
+        // return jsonwebtokens
+        const payload = {
+            user: {
+                id : user.id,
+            }
+        }
+
+        jwt.sign(payload, 
+            config.get('secretkey'), 
+            {expiresIn : 3600000}, 
+            (err ,token)=>{
+                if(err) throw err;
+                res.json({ token });
+            });
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
 });
 
 module.exports = router;
