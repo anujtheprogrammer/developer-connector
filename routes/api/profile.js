1249e873047f585023b6f6cf5609e6bb4b274cf0
anujtheprogrammer <anujbhatt927@gmail.com>
2021-04-29 18:24:01 +0530

    github repo done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -299,26 +299,25 @@ router.delete('/education/:edu_id', auth , async (req,res)=>{
     }
 });
 
-/*
+
 // @router   GET api/profile/github/:username
 // @desc     get user repo from github
 // @access   public
 router.get("/github/:username",(req,res)=>{
     try {
         const options = {
-            uri : `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&
-            client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            uri : `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
             method : 'GET',
-            Headers : {'user-agent': 'node.js'}
-        }
+            headers : {'user-agent': 'node.js'}
+        };
 
         request(options, (error,response,body)=>{
             if(error) console.error(error);
 
             if(response.statusCode !== 200){
-                res.status(400).json({ msg : 'NO github profile found'});
+                return res.status(404).json({ msg : 'NO github profile found'});
             }
-
             res.json(JSON.parse(body));
         });
     } catch (err) {
@@ -327,6 +326,6 @@ router.get("/github/:username",(req,res)=>{
     }
 
 })
-*/
+
 
 module.exports = router;
