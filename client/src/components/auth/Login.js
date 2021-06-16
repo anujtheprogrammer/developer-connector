ed1565e48e9d8a553691acfc17ddb635c049aa0e
anujtheprogrammer <anujbhatt927@gmail.com>
2021-06-16 17:17:57 +0530

    changed

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
+    const [formData,setFormdata] = useState({
+    
+        email:'',
+        password:'',
+
+    });
+
+    const {email, password} = formData;
+
+    const onChange = e => 
+        setFormdata({ ...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        
+            console.log('sucess');
+        
+    }
+
     return (
-        <div>
-            Login
+        <Fragment>
+        <h1 className="large text-primary">
+        Sign In
+      </h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign in Your Account</p>
+      <form onSubmit={e => onSubmit(e)} className="form">
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
         </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} minlength="6" />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+        </Fragment>
+        
     )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
 
 const Register = () => {
     const [formData,setFormdata] = useState({
@@ -13,12 +14,12 @@ const Register = () => {
     const onChange = e => 
         setFormdata({ ...formData, [e.target.name]: e.target.value});
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if(password2 !== password){
             console.log('password do not match');
         }else {
-            console.log(formData);
+            console.log('sucess');
         }
     }
 
@@ -48,7 +49,7 @@ const Register = () => {
         <input type="submit" value="Register" className="btn btn-primary" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
         </Fragment>
         
