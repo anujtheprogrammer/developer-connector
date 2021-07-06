39e3ea16a056b2e1fc84de4af41addee612ff5a5
anujtheprogrammer <anujbhatt927@gmail.com>
2021-07-06 13:14:14 +0530

    almost done

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types'
 
 // get posts
@@ -113,6 +115,51 @@ export const getPost = id => async dispatch => {
             type : GET_POST,
             payload : res.data
         });
+    } catch (err) {
+        dispatch({
+            type : POST_ERROR,
+            payload : {msg : err.response.statusText, status : err.response.status}
+        });
+    }
+}
+
+// add comment
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    }
+
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type : ADD_COMMENT,
+            payload : res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'))
+    } catch (err) {
+        dispatch({
+            type : POST_ERROR,
+            payload : {msg : err.response.statusText, status : err.response.status}
+        });
+    }
+}
+
+// delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type : REMOVE_COMMENT,
+            payload : commentId
+        });
+
+        dispatch(setAlert('Comment Removed', 'success'))
     } catch (err) {
         dispatch({
             type : POST_ERROR,
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,8 @@ import Spinner from '../layout/Spinner'
 import { Link } from 'react-router-dom'
 import { getPost } from '../../actions/post'
 import PostItem from '../posts/PostItem'
+import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 import PropTypes from 'prop-types'
 
 const Post = ({getPost , post : {post, loading}, match}) => {
@@ -16,6 +18,12 @@ const Post = ({getPost , post : {post, loading}, match}) => {
             Back To Posts
         </Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
+        <div className="comments">
+        {post.comments.map(comment => (
+            <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+        </div>
     </Fragment>
 }
 
